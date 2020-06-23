bb30224283a800d3690ee64909d3c88f66cf66fc
Moe <moepua0123@gmai.com>
2020-06-23 21:28:03 +0900

    const.swiftファイルの作成と投稿ボタンの処理まで

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 西山萌花 on 2020/06/23.
+//  Copyright © 2020 moeka.nishiyama. All rights reserved.
+//
+
+import Foundation
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
